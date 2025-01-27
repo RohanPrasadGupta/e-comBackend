b254d10f46b862ffb9d6b29212ce5f27a59f1b52
Rohan <rohanprasadgupta4@gmail.com>
2025-01-27 16:03:09 +0700

    added JWT and route for user login and signup

---
+++ services/middleWare.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+
+const secretKey = "secretKey1234";
+
+exports.verifyToken = (req, res, next) => {
+  const token = req.cookies.cookie;
+  if (!token) {
+    return res.status(401).json({
+      status: "fail",
+      message: "Unauthorized access, no token provided",
+    });
+  }
+  try {
+    const decoded = jwt.verify(token, secretKey);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    if (error.name === "TokenExpiredError") {
+      return res.status(401).json({
+        status: "fail",
+        message: "Token expired, please log in again",
+      });
+    }
+    res.status(401).json({
+      status: "fail",
+      message: "Invalid token",
+    });
+  }
+};
--- services/userServices.js
+++ services/userServices.js
@@ -1,10 +1,11 @@
 const UserModel = require("../models/userModel");
 const bycrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const saltRounds = 10;
-const userHash = process.env.BYCRYPT_HASH;
+const secretKey = "secretKey1234";
 
-exports.addUser = async (req, res) => {
+exports.signUp = async (req, res) => {
   try {
     const { password } = req.body;
     const hashedPassword = await bycrypt.hash(password, saltRounds);
@@ -24,11 +25,11 @@ exports.addUser = async (req, res) => {
   }
 };
 
-exports.getUSer = async (req, res) => {
+exports.signIn = async (req, res) => {
   try {
     const { email, password } = req.body;
-    console.log(email, password);
 
+    console.log(email, password);
     const userDetails = await UserModel.findOne({ email }).select("+password");
     if (!userDetails) {
       return res.status(404).json({
@@ -36,8 +37,6 @@ exports.getUSer = async (req, res) => {
         message: "User not found",
       });
     }
-
-    console.log(userDetails);
 
     const isPasswordMatch = await bycrypt.compare(
       password,
@@ -49,10 +48,35 @@ exports.getUSer = async (req, res) => {
         message: "User id or password is incorrect",
       });
     }
+
+    const token = jwt.sign(
+      {
+        id: userDetails._id,
+        email: userDetails.email,
+        name: userDetails.name,
+      },
+      secretKey,
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    res.cookie("cookie", token, {
+      httpOnly: true,
+      sameSite: "strict",
+      maxAge: 60 * 1000 * 60 * 24, // 1 day,
+    });
+
     res.status(200).json({
       status: "success",
+      message: "Logged in successfully",
       data: {
-        userDetails,
+        userDetails: {
+          id: userDetails._id,
+          email: userDetails.email,
+          name: userDetails.name,
+        },
+        token,
       },
     });
   } catch (error) {
