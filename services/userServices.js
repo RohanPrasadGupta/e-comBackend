045ed940a73730efba25abee2516e1bdf31ee7e9
Rohan <rohanprasadgupta4@gmail.com>
2025-01-31 15:43:50 +0700

    admin login and all order access to admin only

--- services/userServices.js
+++ services/userServices.js
@@ -1,6 +1,7 @@
 const UserModel = require("../models/userModel");
 const bycrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const { checkout } = require("../routes/users/userRoutes");
 
 const saltRounds = 10;
 const secretKey = "secretKey1234";
@@ -40,6 +41,7 @@ exports.signIn = async (req, res) => {
       password,
       userDetails.password
     );
+
     if (!isPasswordMatch) {
       return res.status(404).json({
         status: "fail",
@@ -81,6 +83,74 @@ exports.signIn = async (req, res) => {
     res.status(400).json({
       status: "fail",
       message: error.message,
+    });
+  }
+};
+
+exports.adminLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const userDetails = await UserModel.findOne({ email }).select("+password");
+    if (!userDetails) {
+      return res.status(404).json({
+        status: "fail",
+        message: "User not found",
+      });
+    }
+
+    const isPasswordMatch = await bycrypt.compare(
+      password,
+      userDetails.password
+    );
+
+    if (!isPasswordMatch) {
+      return res.status(404).json({
+        status: "fail",
+        message: "User id or password is incorrect",
+      });
+    } else if (!userDetails.isAdmin) {
+      return res.status(404).json({
+        status: "fail",
+        message: "Login Failed!",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: userDetails._id,
+        email: userDetails.email,
+        name: userDetails.name,
+        isAdmin: true,
+      },
+      secretKey,
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    res.cookie("cookie", token, {
+      httpOnly: true,
+      sameSite: "strict",
+      maxAge: 60 * 1000 * 60 * 24, // 1 day,
+    });
+
+    res.status(200).json({
+      status: "success",
+      message: "Logged in successfully",
+      data: {
+        userDetails: {
+          id: userDetails._id,
+          email: userDetails.email,
+          name: userDetails.name,
+        },
+        token,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error.message,
+      check: "admin",
     });
   }
 };
